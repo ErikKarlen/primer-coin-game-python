bd6f4cb1d4f4d6b51015a33bb5ea356ae594e1e5
Erik Karlén <erik.karlen@gmail.com>
2022-04-15 17:42:01 +0200

    Short package summary

--- primercoingame/__init__.py
+++ primercoingame/__init__.py
@@ -1,1 +1,9 @@
+"""
+Python Primer Coin Game
+=======================
+Provides
+  * A Python implementation of Primer's coin game useful for simulating
+    and testing different strategies.
+
+"""
 from .primer_coin_game import PrimerCoinGame
